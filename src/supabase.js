5987d3aa11d4548b14ae045952c4f91f72e8785c
shimoo81 <wessam75sabry@gmail.com>
2026-09-07 13:05:45 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://jkmdxdajqrgwgytecpgi.supabase.co/rest/v1/";
+const supabaseUrl = "https://jkmdxdajqrgwgytecpgi.supabase.co";
 const supabaseKey = "sb_publishable__JiVQ4_oQDkGs6n8R74CmQ_h5PCyKqc";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
